bc5d2d97412c815c80e97bbd04537df24402ee61
r3a10god <57110219+r3a10god@users.noreply.github.com>
2020-09-02 03:09:15 +0530

    Update bot.py
    Ah! I promise I'll install the git integration...but it'll take time! I'm a student for christ's sake! And an Indian one at that too! Do you even know how hard it is to find the time to learn about bot dev-ing? I'll make do with ctrl+c and ctrl+v for now.  (copy pasting my own code...just in case you misunderstood)

--- bot.py
+++ bot.py
@@ -1,29 +1,55 @@
 import discord
 from os import getenv
 from dotenv import load_dotenv
-from discord.ext import commands
+from discord.ext import commands  # for using commands.BOT() instead of discord.Client()
 
 
 load_dotenv()  # For parsing the .env file
-TOKEN  = getenv('TOKEN')  # Get TOKEN
-client = commands.Bot(command_prefix='!')  # connection to discord (through the command section module)
+TOKEN = getenv('TOKEN')  # Get TOKEN
+ID = int(getenv('ID'))
+bot = commands.Bot(command_prefix='!')  # connection to discord (through the command section module)
 #  Note class discord.ext.commands.Bot() is a subclass of discord.client
 
-@client.event
+@bot.event
 async def on_ready():
-	print(f'Log in successful as {client.user}')  # print message on console
+	print(f'Log in successful as {bot.user}')  # bot.user is the "bot"
 
 
-@client.command()
+@bot.command()  # command information--do later
 async def info(ctx):
 	embed_var = discord.Embed(title="Test", color=0x6c0101)
 	embed_var.add_field(name="First", value="This is a test", inline=False)
 	embed_var.add_field(name="Second", value="This is another test", inline=False)
 	await ctx.send(embed=embed_var)
 
-
-@client.command()
+#  'ctx' has all the information about the message/command you send
+@bot.command()
 async def ping(ctx):
-	await ctx.send("pong!")
+	'''
+	This command is to check the latency of the bot.
+	btw, 'await' is to stop the async flow of the funct to make it do what I want it to do in the meantime.
+	'''
+	await ctx.send(f"{ctx.message.author.mention}, here's the latency: {bot.latency}")
 
-client.run(TOKEN)
+
+@bot.command()
+async def echo(ctx, *, content:str):  # '*' represents all arguements belonging to 'content' as a str
+	await ctx.send(f"\"{content}\"")
+
+
+@bot.event
+async def on_message(message):
+	if message.author == bot.user:
+		return
+	if message.content.startswith("&hello"):
+		await message.channel.send(f"{message.author.mention} hi!")
+	await bot.process_commands(message)
+
+
+@bot.command()
+async def users(ctx):
+	guild = bot.get_guild(ID)
+	await ctx.send(f"\'{guild.name}\' has {guild.member_count} members (including bots)")
+
+
+bot.run(TOKEN)
