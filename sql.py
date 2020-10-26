a1260a6d13409fb33f64feae146d188b40ca0a33
r3a10god <57110219+r3a10god@users.noreply.github.com>
2020-10-26 20:21:13 +0530

    Add files via upload

--- sql.py
+++ sql.py
@@ -4,7 +4,7 @@ import mysql.connector
 mydb = mysql.connector.connect(
 	host='localhost',
 	user='root',
-	password='<mysql psswd>',
+	password='alphabet',
 	database='discord'
 	)
 
@@ -15,7 +15,22 @@ def CheckIfTableExists():  # for later for error handling
 	'''
 
 
-def CreateCursorAndDb():
-	global mydb
-	mycursor = mydb.cursor()
-	return mycursor, mydb
+def IsOneInputFull(uid, cursor):  # read db to make sure there's only one input per user
+	command = "SELECT msg FROM user_input WHERE uid=%s"
+	cursor.execute(command, (uid,))
+	temp_list = [_ for _ in cursor]
+	if temp_list:
+		return True
+	else:
+		return False
+
+
+def Update(cursor, values:tuple):   # update data in table
+	# note the order at which the values are to be entered
+	command = "UPDATE user_input SET msg=%s, date=%s WHERE uid=%s"
+	cursor.execute(command, values)  # 'value' is a tuple
+
+
+def Insert(cursor, values:tuple):   # insert data into table
+	command = "INSERT INTO user_input VALUES (%s, %s, %s)"
+	cursor.execute(command, values)
