32d558b7b474d9461e6a2c5945fba7402eb23aa6
r3a10god <57110219+r3a10god@users.noreply.github.com>
2020-10-26 20:18:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,33 +1,55 @@
+  # ---------- importing the important libraries --------------- #
 import discord
+import asyncio
 from os import getenv
-from random import randint
-from get_data import regdata
+from get_data import *
+from time import strftime
 from dotenv import load_dotenv
-from discord.ext import commands  # for using commands.BOT() instead of discord.Client()
+from discord.ext import commands  # for using commands.Bot() instead of discord.Client()
+  # ------------------------------------------------------------ #
 
-
+  # ------------------ connection and stuff -------------------- #
 load_dotenv()  # For parsing the .env file
 TOKEN = getenv('TOKEN')  # Get TOKEN
 ID = int(getenv('ID'))
-bot = commands.Bot(command_prefix='!')  # connection to discord (through the command section module)
-#  Note class discord.ext.commands.Bot() is a subclass of discord.client
+intents = discord.Intents.default()
+intents.members = True
+bot = commands.Bot(command_prefix='!', intents=intents)  # connection to discord (through the command section module)
+#  Note: class discord.ext.commands.Bot() is a subclass of discord.client
 
 @bot.event
 async def on_ready():
-	print(f'Log in successful as {bot.user}')  # bot.user is the "bot"
-	bot.change_presence(activity=discord.Game("!help"))  # will display as "playing !help"
+	print(f"Logged in as {bot.user}\n{bot.user.id}\n------")  # bot.user is the 'bot'
+	await bot.change_presence(activity=discord.Game("!help"))  # will display as "playing !help"
+  # ------------------------------------------------------------ #
+
+  # --------------- the main commands -------------------------- #
+@bot.command()  # getting user input to show in console
+async def reg(ctx, d:str, h:str, m:str, s:str, *, content:str):
+    T = int(strftime("%Y%m%d%H%M%S"))
+    T = str(T + int(d + h + m + s))
+    rdata(ctx.author.id, content, T)
+    #asyncio.get_event_loop().run_until_complete(checkDate(ctx.author.id))
+    await checkDate(ctx.author.id)
 
 
-@bot.command()  # command information--do later
-async def info(ctx):
-	embed_var = discord.Embed(title="Test", color=0x6c0101)
-	embed_var.add_field(name="First", value="This is a test", inline=False)
-	embed_var.add_field(name="Second", value="This is another test", inline=False)
-	await ctx.send(embed=embed_var)
-	
-@bot.command()	# getting user input to show in console
-async def register(ctx, *, content:str):
-	regdata(ctx.author, content)
-	
-	
+async def send(uid):
+	'''This function sends the registered message to the user with user_id = uid'''
+	query = qdata(uid)  # get data for user 
+	user = bot.get_user(uid)
+	await user.send(query)
+
+
+async def checkDate(uid):
+	'''This function will continuously check wether the deadline has been reached'''
+	date = qdata(uid, query='date')  # this will return reminder date (timestamp) as int
+	T = int(strftime("%Y%m%d%H%M%S"))  # this will return [current] date (timestamp) as int
+	while T < date:
+		#asyncio.sleep(5)  # this will update 'T' every 5 seconds
+		T = int(strftime("%Y%m%d%H%M%S"))
+	else:
+		await send(uid)  # if either the deadline has reached or has been passed then send(uid)
+		Delete(uid)  # delete the reminder from the database
+  # ------------------------------------------------------------ #
+
 bot.run(TOKEN)
